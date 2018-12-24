9c73c4ad41cd8eec702ec48feecc6471c504539a
js_liuyangchao@163.com <js_liuyangchao@163.com>
2018-12-24 10:27:23 +0800

    Initial commit

---
+++ src/main/java/com/mibo/chapter1/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.mibo.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 继承HttpServlet类，让它成为一个Servlet类
+ * @author js_liuyangchao@163.com
+ * @create 2018-12-22 16:16
+ * @desc
+ **/
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/12/24
+  Time: 9:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+
+<h1>Hello liuyangchao!</h1>
+<h2>当前时间：${currentTime}</h2>
+
+</body>
+</html>
